6b77cb49920446da618669e781bafe9953bee43e
b3rcut7 <84776248+b3rcut7@users.noreply.github.com>
2026-05-03 22:39:48 +0300

    1

--- hal/rtl8812a/rtl8812a_phycfg.c
+++ hal/rtl8812a/rtl8812a_phycfg.c
@@ -39,7 +39,7 @@ int transmit_power_boost = 2;
 // Transmit Power Override
 // This value completely overrides the driver's calculations and uses only one value for all transmissions.
 // Zero disables it. Default: 0
-int transmit_power_override = 0;
+extern int rtw_tx_pwr_idx_override;
 
 /* Manual Transmit Power Control */
 
